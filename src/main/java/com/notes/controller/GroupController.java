0aac2807c841f169d550405381098f2d8ded8160
迎迎 <1712131536@qq.com>
2023-04-13 16:46:18 +0800

    111

---
+++ src/main/java/com/notes/controller/GroupController.java
@@ -0,0 +1,21 @@
+package com.notes.controller;
+
+import com.notes.service.GroupService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/group")
+public class GroupController {
+
+    @Autowired
+    GroupService groupService;
+
+    @GetMapping("/getGroupsName")
+    public String[] getGroups(String account){
+        return groupService.getGroupsName(account);
+    }
+
+
+}
