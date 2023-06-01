3b71acfe2e7deade21f7eeb6f301131daf569550
yying <1712131536@qq.com>
2023-06-01 08:14:48 +0000

    删除文件 src

--- src/main/java/com/notes/mapper/PhotoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.notes.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.notes.domain.Photo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PhotoMapper extends BaseMapper<Photo> {
-}
